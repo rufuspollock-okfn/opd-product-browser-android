9401e7accb7efe0b532044c1aedcdf665f069d6b
Nicolas Pieuchot <nls.pct@gmail.com>
2013-11-08 17:24:13 +0100

    Integrating ZXing barcode scanner

--- src/org/okfn/pod/MainActivity.java
+++ src/org/okfn/pod/MainActivity.java
@@ -6,11 +6,11 @@ import java.util.concurrent.ExecutionException;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,26 +20,28 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 	
 	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	public String gtin;
 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		this.callScan(getCurrentFocus());
 	}
 
 	/** Called when the user clicks the Send button */
 	public void sendMessage(View view) {
 	    // Do something in response to button
 
+        EditText editText = (EditText) findViewById(R.id.edit_gtin);
+        editText.setText(gtin);
+
 	    int value_code = 99;
 	    String json = "";
 
-		EditText editText = (EditText) findViewById(R.id.edit_gtin);
-		String gtin = editText.getText().toString();
-
 		//String imgpp1 = "http://product.okfn.org.s3.amazonaws.com/images/gtin/gtin-309/3095757343101.jpg";
-		//http://localhost/github/products/explorer/product/3095757343101
+		// http://localhost/github/products/explorer/product/3095757343101
 		// http://www.le10sport.com/img/ico/icon-club/as-monaco.png
 		// http://product.okfn.org.s3.amazonaws.com/images/gtin/gtin/309/3095757343101.jpg		
 		//json = "{\"gtin\":{\"code\":\"3095757343101\",\"name\":\"La c\u00f4tes de porc cuite et pr\u00e9par\u00e9e dans le filet\",\"product-line\":\"Charcuterie\",\"weight\":\"2 x 70 g\",\"volume\":null,\"alcool\":null,\"img\":\"http:--product.okfn.org.s3.amazonaws.com-images-gtin-gtin-309-3095757343101.jpg\",\"img-default\":\"http:--www.product-open-data.com-images-coming-soon.jpg\"},\"brand\":{\"code\":\"469\",\"name\":\"Fleury Michon\",\"type\":\"Manufacturer-brand\",\"link\":\"www.fleurymichon.fr\",\"img\":\"http:--product.okfn.org.s3.amazonaws.com-images-brand-00000469.jpg\",\"img-default\":\"http:--www.product-open-data.com-images-coming-soon.jpg\"},\"group\":{\"code\":null,\"name\":null},\"gcp\":{\"code\":\"309575\"},\"gpc\":{\"img\":\"http:--www.product-open-data.com-images-gpc-50000000.jpg\",\"img-default\":\"http:--www.product-open-data.com-images-coming-soon.jpg\",\"segment\":{\"code\":\"50000000\",\"nom\":\"Food-Beverage-Tobacco\"},\"family\":{\"code\":\"50240000\",\"nom\":\"Meat-Poultry\"},\"class\":{\"code\":\"50240100\",\"nom\":\"Meat-Poultry - Prepared-Processed\"},\"brick\":{\"code\":null,\"nom\":null}},\"gepir\":{\"return-code\":{\"code\":\"0\",\"name\":\"No error\"},\"source\":\"GEPIR\",\"gln\":{\"code\":\"3010957500109\",\"name\":\"FLEURY MICHON CHARCUTERIE SAS\",\"address-1\":\"BP 1\",\"address-2\":\"\",\"address-3\":\"\",\"cp\":\"85700\",\"city\":\"POUZAUGES\",\"country\":\"FR\"}}}";
@@ -95,10 +97,7 @@ public class MainActivity extends Activity {
 			Toast.makeText(this,"No Internet Connection",1000).show();
 			break;
 	}
-		
 
-		
-		
 	}
 
 	
@@ -119,8 +118,22 @@ public class MainActivity extends Activity {
 		AboutMenuItem.setTitle("POD browser - version "+pinfo.versionName);
 		return true;
 	}
-	
-	
+
+	public void callScan(View view) {
+		IntentIntegrator integrator = new IntentIntegrator(this);
+		integrator.initiateScan();
+	}
+
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+		if(resultCode != RESULT_CANCELED){
+			if (scanResult != null) {
+				gtin = intent.getStringExtra("SCAN_RESULT");
+				gtin = ("0000000000000"+gtin).substring(gtin.length());
+				this.sendMessage(getCurrentFocus());
+			} // end 'scanResult != null'
+		} // end 'resultCode != RESULT_CANCELED'
+	} // end 'onActivityResult'
 
 
 }
